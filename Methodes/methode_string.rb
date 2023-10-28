65a28926068501cadbee2168e8fa0cf6432327e3
Scrimali Anthony <anthony.scrimali@gmail.com>
2023-10-28 14:59:31 +0200

    Ajout du fichier de référencement des méthodes string

---
+++ Methodes/methode_string.rb
@@ -0,0 +1,11 @@
+print
+# Affiche une valeur
+
+puts
+# Affiche une valeur et saute une ligne
+
+gets
+# Demander à l'utilisateur d'entrer une valeur
+
+.chomp
+# Supprime un caractère de fin de ligne
