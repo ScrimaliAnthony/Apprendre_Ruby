c00a268d8025af301482e30493526d832e050db3
Scrimali Anthony <anthony.scrimali@gmail.com>
2023-10-29 15:38:28 +0100

    Exercice 1 optimisé

--- Exercices/Conditions/exercice_01.rb
+++ Exercices/Conditions/exercice_01.rb
@@ -4,17 +4,14 @@ print "Entrez 3 nombres différents\n"
 
 print "Premier nombre : "
 NB_1 = gets.chomp.to_i
+nb_max = NB_1
 
 print "Deuxième nombre : "
 NB_2 = gets.chomp.to_i
+nb_max = NB_2 if NB_2 > nb_max
 
 print "Troisième nombre : "
 NB_3 = gets.chomp.to_i
+nb_max = NB_3 if NB_3 > nb_max
 
-if NB_1 > NB_2 && NB_1 > NB_3
-  print NB_1
-elsif NB_2 > NB_1 && NB_2 > NB_3
-  print NB_2
-else
-  print NB_3
-end
+print nb_max
