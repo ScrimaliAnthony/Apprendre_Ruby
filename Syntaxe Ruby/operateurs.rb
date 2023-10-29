13c08dcda9aabab29ab78bec94d9f85bd9911a4e
Scrimali Anthony <anthony.scrimali@gmail.com>
2023-10-29 16:59:17 +0100

    Ajout du fichier listant les opérateur

---
+++ Syntaxe Ruby/operateurs.rb
@@ -0,0 +1,17 @@
+# Opérateurs de base
+
+=   # Opérateur d'afectation
++   # Addition
+-   # Soustraction
+*   # Multiplication
+/   # Division
+%   # Modulo
+
+# Opérateurs de comparaison
+
+<   # Strictement inférieur à
+>   # Strictement supérieur à
+<=  # Inférieur ou égale à
+>=  # Supérieur ou égale à
+==  # Égale à
+!=  # Différent de
