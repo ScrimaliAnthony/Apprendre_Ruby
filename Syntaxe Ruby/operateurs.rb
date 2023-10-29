9001799f9474713efc9c4f31f32c649d58d37f21
Scrimali Anthony <anthony.scrimali@gmail.com>
2023-10-29 17:31:07 +0100

    Mise à jour de la liste d'opérateurs

--- Syntaxe Ruby/operateurs.rb
+++ Syntaxe Ruby/operateurs.rb
@@ -1,11 +1,11 @@
-# Opérateurs de base
+# Opérateurs arithmétiques
 
-=   # Opérateur d'afectation
 +   # Addition
 -   # Soustraction
 *   # Multiplication
 /   # Division
 %   # Modulo
+**  # Exponentiation
 
 # Opérateurs de comparaison
 
@@ -15,3 +15,30 @@
 >=  # Supérieur ou égale à
 ==  # Égale à
 !=  # Différent de
+<=> # Comparaison (retourne -1 si inferieur à, 0 si égale à et 1 si supérieur à)
+=== # Utilisé dans la condition case, son comportement varie en fonction des classes des objets qu'il compare
+    # nombres   : agit comme l'opérateur ==
+    # string    : agit comme l'opérateur ==
+    # regex     : vérifie si la chaîne correspond à l'expression régulière
+    # range     : vérifie si la valeur donnée se situe dans la plage
+
+# Opérateurs d'afectation
+
+=   # Afectation
++=  # Incrémentation
+-=  # décrémentation
+*=  # opérateur d'assignation de multiplication
+/=  # opérateur d'assignation de division
+%=  # opérateur d'assignation de modulo
+**= # opérateur d'assignation de puissance
+
+# Opérateurs logiques
+
+&&  # ET
+||  # OU
+!   # Négation
+
+# Opérateur de plage
+
+..  # Plage inclusive
+... # Plage exclusive
